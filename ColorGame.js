19b04a67d6671882148ca50616aee89aca8c1448
BarbedWire <barbedwire@BarbedWires-MacBook-Pro.local>
2017-09-12 22:48:11 +0300

    Stage 2. Random color selection

--- ColorGame.js
+++ ColorGame.js
@@ -8,8 +8,9 @@ var colors = [
 ];
 
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.getElementById("message");
 
 colorDisplay.textContent = pickedColor;
 
@@ -18,10 +19,22 @@ for (var i = 0; i < squares.length; i++) {
   squares[i].addEventListener("click", function() {
     var selectedColor = this.style.backgroundColor;
     if (selectedColor === pickedColor) {
-      alert("GUESS");
+      messageDisplay.textContent = "Correct!";
+      changeColors(pickedColor);
     } else {
-      alert("WRONG");
+      this.style.backgroundColor = "#232323";
+      messageDisplay.textContent = "Try again";
     }
   })
+}
+
+function changeColors(color) {
+  for (var i = 0; i < squares.length; i++) {
+    squares[i].style.backgroundColor = color;
+  }
+}
+
+function pickColor() {
+  return colors[Math.floor(Math.random() * colors.length + 1)];
 }
 
