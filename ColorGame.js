2cc80214fabd902c5158be0848434ee9d0775dc6
BarbedWire <barbedwire@BarbedWires-MacBook-Pro.local>
2017-09-13 09:38:18 +0300

    Stage 3. Random colors selection

--- ColorGame.js
+++ ColorGame.js
@@ -1,16 +1,10 @@
-var colors = [
-  "rgb(255, 0, 0)",
-  "rgb(255, 255, 0)",
-  "rgb(0, 255, 0)",
-  "rgb(0, 255, 255)",
-  "rgb(0, 0, 255)",
-  "rgb(255, 0, 255)"
-];
+var colors = genarateRandomColors(6);
 
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.getElementById("message");
+var h1 = document.querySelector("h1");
 
 colorDisplay.textContent = pickedColor;
 
@@ -32,9 +26,29 @@ function changeColors(color) {
   for (var i = 0; i < squares.length; i++) {
     squares[i].style.backgroundColor = color;
   }
+  h1.style.backgroundColor = color;
+}
+
+function genarateRandomColors(num) {
+  var arr = [];
+  for (var i = 0; i < num; i++) {
+    arr.push(randomColor());
+  } 
+  return arr;
 }
 
 function pickColor() {
-  return colors[Math.floor(Math.random() * colors.length + 1)];
+  return colors[getRandomNumber(colors.length) + 1];
+}
+
+function randomColor() {
+  var r = getRandomNumber(256);
+  var g = getRandomNumber(256);
+  var b = getRandomNumber(256);
+  return "rgb(" + r + ", " + g + ", " + b + ")";
+}
+
+function getRandomNumber(upperBound) {
+  return Math.floor(Math.random() * upperBound); 
 }
 
